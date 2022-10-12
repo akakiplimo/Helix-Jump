49a50a5330df03b67433073629637a70416f7f5f
A. Kiplimo <akakiplimo@gmail.com>
2022-10-13 00:13:02 +0300

    Helix Manager Script

---
+++ Assets/Scripts/HelixManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelixManager : MonoBehaviour
+{
+    public GameObject[] helixRings;
+    public float ySpawn = 0;
+    public float ringsDistance = 5;
+
+    public int noOfRings = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Spawn helix rings
+        for (int i = 0; i < noOfRings; i++)
+        {
+            if (i == 0)
+                SpawnRing(0);
+            else
+                SpawnRing(Random.Range(1, helixRings.Length - 1));
+        }
+
+        // Spawn the last ring
+        SpawnRing(helixRings.Length - 1);
+    }
+
+
+    public void SpawnRing(int ringIndex)
+    {
+        GameObject gameRings = Instantiate(helixRings[ringIndex], transform.up * ySpawn, Quaternion.identity);
+        gameRings.transform.parent = transform;
+        ySpawn -= ringsDistance;
+    }
+}
